74251a4ff088374e70a5eccfdbb5bff34f1d3a0f
John Doe <rushilshah565@gmail.com>
2023-12-24 00:35:16 +0530

    gsheet backend added

--- venv/app.py
+++ venv/app.py
@@ -1,5 +1,19 @@
 import tkinter as tk
 import requests, keyboard
+import pandas as pd
+import pynput, gspread
+from oauth2client.service_account import ServiceAccountCredentials
+scope = ['https://www.googleapis.com/auth/spreadsheets']
+credentials = ServiceAccountCredentials.from_json_keyfile_name('venv/gcred.json', scope)
+gc = gspread.authorize(credentials)
+sht1 = gc.open_by_key('1WoKtTVgOY9EIQjvhG-H4KsyRxN01nh234-FRAeb8_Ks')
+worksheet = sht1.get_worksheet(0)
+
+SHEET_ID = '1WoKtTVgOY9EIQjvhG-H4KsyRxN01nh234-FRAeb8_Ks'
+SHEET_NAME = 'GATHAS'
+url = f'https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}'
+df = pd.read_csv(url)
+row_index_global = None 
 
 class MyApp:
     def __init__(self, root):
@@ -12,11 +26,12 @@ class MyApp:
         self.root.config(bg="darkblue")
 
         # Set initial time (15 minutes in seconds)
-        self.remaining_time = 15 * 60
+        self.remaining_time = 0.1 * 60
 
         # Disable keyboard activity
         self.root.bind("<Key>", lambda e: "break")
-        
+        self.root.unbind("<Button-1>")  # Unbind left mouse click
+        self.root.bind("<B1-Motion>", lambda e: "break")  # Unbind left mouse drag
         # Create and place widgets
         # Disable the Windows key
         self.disable_windows_key()
@@ -43,22 +58,24 @@ class MyApp:
         self.text_widget.pack(pady=200, padx=200, ipady=100, ipadx=100)
         # Insert text into the read-only text widget with consistent padding on each line
         padding_text = " " * 10  # Add extra spaces for padding
-        lines = [
-            "सकलाऽऽर्हत् – प्रतिष्ठान~मधिष्ठानं शिव – श्रियः ।",
-            "भूर् – भुव :-स्वस् – त्रयीशान~मार्हन्त्यं प्रणिदध्महे"
-        ]
-
+        # print(df.head)
+        # paragraph = df.iat[1, 0] 
+        row_index = df[df['Done or not'].isna()].index[0]
+        global row_index_global
+        row_index_global = row_index
+        paragraph = df.at[row_index, 'Gatha']        
+        lines = paragraph.split('।')
         for line in lines:
             self.text_widget.config(state=tk.NORMAL)
             self.text_widget.insert(tk.END, padding_text + line + "\n")
             self.text_widget.config(state=tk.DISABLED)
 
- # Button to exit the application
+        # Button to exit the application
         exit_button = tk.Button(self.root, text="Exit", command=self.exit_application, state=tk.DISABLED)
         exit_button.place(x=self.root.winfo_screenwidth() - 790, y=700)
 
         # Schedule the activation of the exit button after 15 seconds
-        self.root.after(1500, lambda: exit_button.config(state=tk.NORMAL))
+        self.root.after(int(self.remaining_time), lambda: exit_button.config(state=tk.NORMAL))
 
         self.update_timer()
         # Fetch and display text from API
@@ -103,11 +120,23 @@ class MyApp:
 
             # Call the update_timer method after 1000 milliseconds (1 second)
             self.root.after(1000, self.update_timer)
+
         else:
             # Timer reached 0, perform any action or update the UI as needed
             self.timer_label.config(text="Time's up!")
+            # pynput.mouse.Listener(suppress=False)
+            mouse_listener.stop()
+            row_index_local = df[df['Done or not'].isna()].index[0]
+            cell_address = f'B{row_index_local + 2}'
+            # print(row_index_local)
+            # df.at[row_index_local, 'Done or not'] = 'Done'
+            # print(df.at[row_index_local, 'Done or not'])
+            worksheet.update_acell(cell_address, 'Done')
+
 
 if __name__ == "__main__":
+    mouse_listener = pynput.mouse.Listener(suppress=True)
+    mouse_listener.start()
     root = tk.Tk()
     app = MyApp(root)
     root.mainloop()
